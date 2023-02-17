239158d24d80bdfa45f434fe9f15a3774377e616
koboboy <stephenezenwa96@gmail.com>
2023-02-16 20:41:58 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-
 #include <stdio.h>
 /**
- * main - prints with proper grammer
- *
- * Return: always return 0.
- */
+* main - A program that prints a line with puts function
+* Return: 0 (success)
+*/
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			/* always return 0*/
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
